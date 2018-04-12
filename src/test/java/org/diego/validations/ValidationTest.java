2e540bcca59c83b7c92b108376c051e7ba30ac9f
Diego Palomo <diego.palomo@momit.com>
2018-04-12 22:09:50 +0200

    Complete validation test

--- src/test/java/org/diego/validations/ValidationTest.java
+++ src/test/java/org/diego/validations/ValidationTest.java
@@ -5,10 +5,13 @@ import static org.junit.Assert.assertThat;
 
 import java.util.ArrayList;
 
+import org.diego.dto.FlightSearchRequest;
 import org.diego.exception.FlightSearchException;
 import org.junit.Test;
 
 public class ValidationTest {
+    
+    FlightSearchRequest flightReques = new FlightSearchRequest(null, null, 0, 0, 0, 0);
     
     @Test
     public void testAssertFalseValidationEmptyList() {
@@ -19,4 +22,47 @@ public class ValidationTest {
             assertThat(e.getCauseMessage(), is("Empty List"));
         }
     }
+    
+    @Test
+    public void testAssertFalseValidationNullList() {
+        try {
+            CommonValidations.validateEmptyList(null, 404, "Null List");
+        } catch (FlightSearchException e) {
+            assertThat(e.getErrorCode(), is(404));
+            assertThat(e.getCauseMessage(), is("Null List"));
+        }
+    }
+    
+    @Test
+    public void testAssertFalseValidationNullPointer() {
+        try {
+            CommonValidations.validateNullPointer(null, 404, "Null Pointer");
+        } catch (FlightSearchException e) {
+            assertThat(e.getErrorCode(), is(404));
+            assertThat(e.getCauseMessage(), is("Null Pointer"));
+        }
+    }
+    
+    @Test
+    public void testAssertFalseValidationFlightSearch() {
+        flightReques.setDepartureFrom(0);
+        try {
+            FlightSearchValidations.validateInput(flightReques);
+        } catch (FlightSearchException e) {
+            assertThat(e.getErrorCode(), is(400));
+            assertThat(e.getCauseMessage(), is("The departure is invalid"));
+        }
+    }
+    
+    @Test
+    public void testAssertFalseValidationFlightSearch2() {
+        flightReques.setDepartureFrom(5);
+        try {
+            FlightSearchValidations.validateInput(flightReques);
+        } catch (FlightSearchException e) {
+            assertThat(e.getErrorCode(), is(400));
+            assertThat(e.getCauseMessage(), is("The data is invalid"));
+        }
+    }
+    
 }
